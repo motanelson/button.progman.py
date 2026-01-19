431a1c224a64261a31112c68ddebdd2296fe031c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-19 14:14:18 +0000

    Add files via upload

--- gui.py
+++ gui.py
@@ -90,6 +90,36 @@ class RamDiskBuilderGUI:
             os.system('cp vmlinuz /tmp/root 2> /dev/null')
             os.system('cp initrd.img /tmp/root 2> /dev/null')
             os.system('cp ldlinux.c32 /tmp/root 2> /dev/null')
+            os.system('cp init /tmp/root 2> /dev/null')
+            os.system('cp linuxrc /tmp/root 2> /dev/null')
+            os.system('mkdir /tmp/root/etc 2> /dev/null')
+
+
+            os.system('mkdir /tmp/root/dev 2> /dev/null')
+            os.system('mkdir /tmp/root/bin 2> /dev/null')
+            os.system('mkdir /tmp/root/sbin 2> /dev/null')
+            os.system('mkdir /tmp/root/usr 2> /dev/null')
+            os.system('mkdir /tmp/root/root 2> /dev/null')
+            os.system('mkdir /tmp/root/root/etc 2> /dev/null')
+            os.system('mkdir /tmp/root/root/dev 2> /dev/null')
+            os.system('mkdir /tmp/root/root/bin 2> /dev/null')
+            os.system('mkdir /tmp/root/root/sbin 2> /dev/null')
+            os.system('mkdir /tmp/root/root/usr 2> /dev/null')
+            os.system('mkdir /tmp/root/root/run 2> /dev/null')
+            os.system('mkdir /tmp/root/root/proc 2> /dev/null')
+            os.system('mkdir /tmp/root/root/sys 2> /dev/null')
+            os.system('mkdir /tmp/root/proc 2> /dev/null')
+            os.system('mkdir /tmp/root/sys 2> /dev/null')
+
+            os.system('mkdir /tmp/root/boot 2> /dev/null')
+            os.system('mkdir /tmp/root/run 2> /dev/null')
+            #os.system(' 2> /dev/null')
+
+            #os.system(' 2> /dev/null')
+            #os.system(' 2> /dev/null')
+            #os.system(' 2> /dev/null')
+
+
             os.system('chmod 777 /tmp/root/*')
             
 
